4509ef9d76285eb756ccea5d6cc49d3990f3b89d
papichoolo <souravsahil100@gmail.com>
2024-02-18 23:28:26 +0530

    added stuf

--- init.lua
+++ init.lua
@@ -269,7 +269,6 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
-
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
